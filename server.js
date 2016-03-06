94dcc85f1b10cbc18d7d7f738df36708146b8285
safari137 <dbdill137@gmail.com>
2016-03-06 16:16:40 +0000

    reverting back to throw err

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/:id', function(req, res) {
     var id = req.params.id;
     
     Url.find({_id: id}, function(err, foundUrl) {
-        if (err) { res.send(err);  return; }
+        if (err) { throw err }
         
         if (!foundUrl.length) {
             res.send({'error': 'Invalid shortcut'});
